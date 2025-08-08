68743cc7821179ef0843f52cf26f4c9a422932c1
gilmerperez <gilmer2002@outlook.com>
2025-08-08 16:34:37 -0400

    Added blank basic pages and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
+import Header from "./components/Header/Header";
+import Home from "./pages/Home/Home";
+import Menu from "./pages/Menu/Menu";
+import PartyPacks from "./pages/PartyPacks/PartyPacks";
+import OrderOnline from "./pages/OrderOnline/OrderOnline";
+import Contact from "./pages/Contact/Contact";
+import PrivacyPolicy from "./pages/PrivacyPolicy/PrivacyPolicy";
+import TermsOfService from "./pages/TermsOfService/TermsOfService";
+import NotFound from "./pages/NotFound/NotFound";
+import Footer from "./components/Footer/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <>
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/party-packs" element={<PartyPacks />} />
+            <Route path="/order-online" element={<OrderOnline />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+            <Route path="/terms-of-service" element={<TermsOfService />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </>
+    </>
+  );
 }
 
 export default App;
