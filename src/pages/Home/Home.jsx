eea6a880ce4518742adb644ec19ceeb1359614b4
gilmerperez <gilmer2002@outlook.com>
2025-08-25 17:46:48 -0400

    Added skinny banner section to home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -43,6 +43,44 @@ function Home() {
               </div>
             </div>
           </section>
+
+          {/* Information banner */}
+          <section className={styles.informationBanner}>
+            <div className={styles.bannerContent}>
+              {/* Location */}
+              <div className={styles.infoItem}>
+                <i className="fas fa-map-marker-alt"></i>
+                <h3>LOCATION</h3>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.google.com/maps?sca_esv=e5cef3e958d363d7&rlz=1C1RXQR_enUS1087US1087&uact=5&gs_lp=Egxnd3Mtd2l6LXNlcnAiHmJ1YmJhbG91cyBib2RhY2lvdXMgYmJxIGNvbnJveTICECYyCBAAGIAEGKIEMggQABiiBBiJBTIIEAAYgAQYogQyCBAAGIAEGKIEMgUQABjvBUjlDVDjBli-DHACeAGQAQCYAa8CoAH3B6oBBzAuMy4xLjG4AQPIAQD4AQGYAgegAo0IwgIKEAAYsAMY1gQYR8ICDRAAGLADGNYEGEcYyQPCAg4QABiABBiwAxiSAxiKBcICCBAAGBYYChgewgIGEAAYFhgemAMA4gMEGAAgWuIDBBgAIFviAwQYACBciAYBkAYJkgcHMi4zLjEuMaAHqiSyBwcwLjMuMS4xuAeDCMIHBTAuNi4xyAcS&um=1&ie=UTF-8&fb=1&gl=us&sa=X&geocode=KQekdArSfueIMd2HaKpG8rfY&daddr=5818+Conroy+Rd,+Orlando,+FL+32835"
+                >
+                  5818 Conroy Road, Orlando FL 32835
+                </a>
+              </div>
+
+              {/* Telephone */}
+              <div className={styles.infoItem}>
+                <i className="fas fa-phone"></i>
+                <h3>TELEPHONE</h3>
+                <a href="tel:(407) 295-1212">(407) 295 - 1212</a>
+              </div>
+
+              {/* Hours */}
+              <div className={styles.infoItem}>
+                <i className="fas fa-clock"></i>
+                <h3>HOURS</h3>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.google.com/search?sca_esv=e5cef3e958d363d7&rlz=1C1RXQR_enUS1087US1087&q=bubbalou%27s+bodacious+bbq+conroy&spell=1&sa=X&ved=2ahUKEwj5l7mY7aaPAxUlTDABHbYuJD0QBSgAegQIFxAB&biw=1745&bih=828&dpr=1.1"
+                >
+                  10 AM - 9 PM
+                </a>
+              </div>
+            </div>
+          </section>
         </div>
       </main>
     </>
